1b15b6656ecf18a972fc10113faac39fa5dae89b
hejianxin <hejianxinsir@gmail.com>
2020-06-22 21:12:56 +0800

    把 build 从 .gitignore 中删除

---
+++ build/precache-manifest.f1734d9c632f69261f9459fe6d4082e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20c5e048bb4369a7153bdd512d9f0f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d23f029bbaf47c865a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3ae205fcde901d3f4ac",
+    "url": "/static/js/2.5d264267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d264267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d23f029bbaf47c865a",
+    "url": "/static/js/main.a124b16f.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
